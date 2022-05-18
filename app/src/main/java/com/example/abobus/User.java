f32ae344e0cb3ecd0942479884a9547de986739a
FB <zurapov8@bk.ru>
2022-05-18 22:39:14 +0300

    com

---
+++ app/src/main/java/com/example/abobus/User.java
@@ -0,0 +1,16 @@
+package com.example.abobus;
+
+public class User {
+    public int UserID;
+    public String Name;
+
+
+    public User() {
+
+    }
+
+    public User(int userID, String name) {
+        UserID = userID;
+        Name = name;
+    }
+}
