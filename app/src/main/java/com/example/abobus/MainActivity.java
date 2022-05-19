0dd4bd343f2c10d5badf78eb2d1b4e00894bde91
FB <zurapov8@bk.ru>
2022-05-19 08:46:05 +0300

    Commit!!!

--- app/src/main/java/com/example/abobus/MainActivity.java
+++ app/src/main/java/com/example/abobus/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
+
 
         db = FirebaseDatabase.getInstance().getReference();
 
